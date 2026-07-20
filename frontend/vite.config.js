12562d3be72644001706fee3da869c0af1a02f7a
Haren Luis Silva Lopez <harenluis@gmail.com>
2026-07-20 17:35:19 -0500

    refactor: mover config de vite y package.json a carpeta frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  root: 'frontend',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
